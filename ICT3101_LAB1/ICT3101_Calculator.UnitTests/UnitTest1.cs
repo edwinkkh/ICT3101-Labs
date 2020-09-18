a1c60f66925d66f939994cf155dc1c69a4e6d126
unknown <edwinkkh@gmail.com>
2020-09-18 11:31:40 +0800

    added test cases for other methods

--- ICT3101_LAB1/ICT3101_Calculator.UnitTests/UnitTest1.cs
+++ ICT3101_LAB1/ICT3101_Calculator.UnitTests/UnitTest1.cs
@@ -12,12 +12,39 @@ namespace ICT3101_Calculator.UnitTests
         }
 
         [Test]
-        public void Test1()
+        public void Add_WhenAddingTwoNumbers_ResultEqualToSum()
         {
             // Act
             double result = _calculator.Add(10, 20);
             // Assert
             Assert.That(result, Is.EqualTo(30));
+        }
+
+        [Test]
+        public void Subtract_WhenSubtractTwoNumbers_ResultEqualToSum()
+        {
+            // Act
+            double result = _calculator.Subtract(10, 20);
+            // Assert
+            Assert.That(result, Is.EqualTo(-10));
+        }
+
+        [Test]
+        public void Multiply_WhenMultiplyTwoNumbers_ResultEqualToSum()
+        {
+            // Act
+            double result = _calculator.Multiply(10, 20);
+            // Assert
+            Assert.That(result, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void Divide_WhenDivideTwoNumbers_ResultEqualToSum()
+        {
+            // Act
+            double result = _calculator.Divide(10, 20);
+            // Assert
+            Assert.That(result, Is.EqualTo(0.5));
         }
     }
 }
